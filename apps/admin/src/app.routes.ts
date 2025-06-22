66f61283dd4c086d5a9fbd9011273897f6548fa4
Taner Saydam <taner_and_you@hotmail.com>
2025-06-22 07:12:15 +0300

    (Refactoring) Route yapısını iyileştirelim

--- apps/admin/src/app.routes.ts
+++ apps/admin/src/app.routes.ts
@@ -15,15 +15,7 @@ export const appRoutes: Route[] = [
             },
             {
                 path: 'products',
-                loadComponent: () => import('./pages/products/products')
-            },
-            {
-                path: 'products/create',
-                loadComponent:() => import("./pages/products/create/create")
-            },
-            {
-                path: 'products/edit/:id',
-                loadComponent:() => import("./pages/products/create/create")
+                loadChildren: () => import('./pages/products/routes')
             }
         ]
     }
