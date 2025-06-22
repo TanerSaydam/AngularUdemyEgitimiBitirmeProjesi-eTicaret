7602215d7a1a09822da55510d6a919d67f6a6920
Taner Saydam <taner_and_you@hotmail.com>
2025-06-23 02:38:30 +0300

    Login sayfasını oluşturalım

--- apps/admin/src/pages/login/login.ts
+++ apps/admin/src/pages/login/login.ts
@@ -1,11 +1,35 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ChangeDetectionStrategy, Component, inject, ViewEncapsulation } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { UserModel } from '../users/users';
+import { FlexiToastService } from 'flexi-toast';
+import { Router } from '@angular/router';
 
 @Component({
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './login.html',
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export default class Login {
+  readonly #http = inject(HttpClient);
+  readonly #toast = inject(FlexiToastService);
+  readonly #router = inject(Router);
 
+  signIn(form: NgForm){
+    if(!form.valid) return;
+
+    const endpoint = `api/users?userName=${form.value['userName']}&password=${form.value['password']}`;
+    this.#http.get<UserModel[]>(endpoint).subscribe(res => {
+      if(res.length === 0){
+        this.#toast.showToast("Hata","Kullanıcı adı ya da şifre yanlış","error");
+        return;
+      }
+
+      localStorage.setItem("response",JSON.stringify(res[0]));
+      this.#router.navigateByUrl("/");
+    });
+  }
 }
