e0c8211f53f35d697d97d3aeb652a2b5c58960c6
Taner Saydam <taner_and_you@hotmail.com>
2025-06-22 19:24:32 +0300

    Kullanıcılar sayfasını oluşturalım ve listesini gösterelim

---
+++ apps/admin/src/pages/users/users.ts
@@ -0,0 +1,57 @@
+import { HttpClient, httpResource } from '@angular/common/http';
+import { ChangeDetectionStrategy, Component, computed, inject, ViewEncapsulation } from '@angular/core';
+import Blank from '../../components/blank';
+import { FlexiGridModule } from 'flexi-grid';
+import { RouterLink } from '@angular/router';
+import { FlexiToastService } from 'flexi-toast';
+
+export interface UserModel{
+  id?: string;
+  firstName: string;
+  lastName: string;
+  fullName: string;
+  userName: string;
+  email: string;
+  password: string;
+  isAdmin: boolean;
+}
+
+export const initialUser:UserModel = {
+  firstName: "",
+  lastName: "",
+  userName: "",
+  email: "",
+  password: "",
+  isAdmin: false,
+
+  get fullName(): string {
+    return `${this.firstName} ${this.lastName}`;
+  }
+}
+
+@Component({
+  imports: [
+    Blank,
+    FlexiGridModule,
+    RouterLink
+  ],
+  templateUrl: './users.html',
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export default class Users {
+  readonly result = httpResource<UserModel[]>(() => "api/users");
+  readonly data = computed(() => this.result.value() ?? []);
+  readonly loading = computed(() => this.result.isLoading());
+
+  readonly #toast = inject(FlexiToastService);
+  readonly #http = inject(HttpClient);
+
+  delete(id: string){
+    this.#toast.showSwal("Kullanıcıyı Sil?","Kullanıcı silmek istiyor musunuz?","Sil",() => {
+      this.#http.delete(`api/users/${id}`).subscribe(()=> {
+        this.result.reload();
+      })
+    })
+  }
+}
