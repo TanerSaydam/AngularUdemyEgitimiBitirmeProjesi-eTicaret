386639c63e925135b001f774ec0d933446062f5f
Taner Saydam <taner_and_you@hotmail.com>
2025-06-23 14:19:13 +0300

    Ödeme yapalım ve sipariş oluşturalım

--- apps/ui/src/pages/payment/payment.ts
+++ apps/ui/src/pages/payment/payment.ts
@@ -1,14 +1,17 @@
-import { httpResource } from '@angular/common/http';
-import { ChangeDetectionStrategy, Component, computed, inject, ViewEncapsulation } from '@angular/core';
+import { HttpClient, httpResource } from '@angular/common/http';
+import { ChangeDetectionStrategy, Component, computed, inject, signal, ViewEncapsulation } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Common } from '../../services/common';
 import { BasketModel } from '@shared/models/basket.model';
 import { TrCurrencyPipe } from 'tr-currency';
+import { OrderModel, initialOrder } from '@shared/models/order.model';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   imports: [
     RouterLink,
-    TrCurrencyPipe
+    TrCurrencyPipe,
+    FormsModule
   ],
   templateUrl: './payment.html',
   encapsulation: ViewEncapsulation.None,
@@ -16,16 +19,32 @@ import { TrCurrencyPipe } from 'tr-currency';
 })
 export default class Payment {
   readonly result = httpResource<BasketModel[]>(() => `api/baskets?userId=${this.#common.user()!.id}`);
-  readonly data = computed(() => this.result.value() ?? []);
+  readonly baskets = computed(() => this.result.value() ?? []);
   readonly total = computed(() => {
     let val = 0;
-    this.data().forEach(res => {
+    this.baskets().forEach(res => {
       val+= res.productPrice * res.quantity
     });
 
     return val;
   });
   readonly kdv = computed(() => this.total() * 18 / 100);
+  readonly data = signal<OrderModel>(initialOrder);
+  readonly showSuccessPart = signal<boolean>(false);
 
   readonly #common = inject(Common);
+  readonly #http = inject(HttpClient);
+
+  pay(form: NgForm){
+    if(!form.valid) return;
+
+    this.data.update(prev => ({
+      ...prev,
+      userId: this.#common.user()!.id!,
+      baskets: [...this.baskets()]
+    }));
+    this.#http.post("api/orders", this.data()).subscribe(res => {
+
+    });
+  }
 }
